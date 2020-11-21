a8ce2ca80fe7486fe14d2f5eff3bd5fd384f96e0
Ken Tauchi <kentauchica@gmail.com>
2020-11-20 20:14:32 -0800

    change server address for backend

--- src/global_variable.js
+++ src/global_variable.js
@@ -1,2 +1,2 @@
-export const API_URL = `https://www.langara-app.ca`; // PRODUCTION
+export const API_URL = `http://3.219.13.26`; // PRODUCTION
 // export const API_URL = `http://localhost:8888/langara_web`; // DEVELOPMENT
