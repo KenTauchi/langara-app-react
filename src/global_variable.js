c2d4214e7824409243f7ffe24e88ade3342d26d1
Ken Tauchi <kentauchica@gmail.com>
2020-11-22 19:35:47 -0800

    change API URL

--- src/global_variable.js
+++ src/global_variable.js
@@ -1,2 +1,2 @@
-export const API_URL = `https://api.langara-app.ca/index.php`; // PRODUCTION
+export const API_URL = `https://api.langara-app.ca`; // PRODUCTION
 // export const API_URL = `http://localhost:8888/langara_web`; // DEVELOPMENT
