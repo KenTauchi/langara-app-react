a6b9f14a1e9c36a132fa9c79219641d9b9cabcec
Ken Tauchi <kentauchica@gmail.com>
2021-01-30 22:04:29 -0800

    modify student work header description, swiper bullet placement and size

--- src/component/student-work/student-work-top-intro/StudentWorkTopIntro.js
+++ src/component/student-work/student-work-top-intro/StudentWorkTopIntro.js
@@ -7,12 +7,12 @@ export default function StudentWorkTopIntro(props) {
   return (
     <div className="studentwork-top-intro">
       <h1>{props.acf.student_work_front_title}</h1>
-      <p
-        className="intro-desc"
-        dangerouslySetInnerHTML={{
-          __html: props.acf.student_work_front_description,
-        }}
-      ></p>
+      <p className="intro-desc">
+        The Web and Mobile Design and Development post-degree diploma at Langara
+        College (in Vancouver, Canada) is an intensive four-term program that
+        prepares students with no previous experience for a career in the
+        industry.
+      </p>
       <StudentWork />
       <Link to="/projects">See More</Link>
     </div>
