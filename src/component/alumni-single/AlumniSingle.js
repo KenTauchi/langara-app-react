7b01355c4e609fd3a61741de30a716b016ea0995
Ken Tauchi <kentauchica@gmail.com>
2021-03-10 13:49:20 -0800

    add movies

--- src/component/alumni-single/AlumniSingle.js
+++ src/component/alumni-single/AlumniSingle.js
@@ -4,25 +4,21 @@ import "./_AlumniSingle.scss";
 export default function AlumniSingle(props) {
   return (
     <div className="alumna">
-      {props.acf.alumni_name === "Andre Luiz Falco" ? (
-        <p>Coming Soon...</p>
-      ) : (
-        <div>
-          <div className="alumna-desc">
-            <h2>{props.acf.alumni_name}</h2>
-            <p>
-              {props.acf.job_position} at {props.acf.company}
-            </p>
-            <p className="stream">Graduate of WMDD {props.acf.stream} Stream</p>
-            <p className="year">
-              ({props.acf.starting_year} - {props.acf.graduation_year})
-            </p>
-          </div>
-          <div className="alumna-movie">
-            <iframe src={props.acf.movie_link} title="alumna-movie"></iframe>
-          </div>
+      <div>
+        <div className="alumna-desc">
+          <h2>{props.acf.alumni_name}</h2>
+          <p>
+            {props.acf.job_position} at {props.acf.company}
+          </p>
+          <p className="stream">Graduate of WMDD {props.acf.stream} Stream</p>
+          <p className="year">
+            ({props.acf.starting_year} - {props.acf.graduation_year})
+          </p>
         </div>
-      )}
+        <div className="alumna-movie">
+          <iframe src={props.acf.movie_link} title="alumna-movie"></iframe>
+        </div>
+      </div>
     </div>
   );
 }
